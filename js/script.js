defb5c6d758fe4544af181bea845f65a000fbaa8
Tomicz Engineering LLC <tomiczdarko@gmail.com>
2024-09-10 04:15:40 +0200

    Added navbar backgorund scroll background change on scroll

--- js/script.js
+++ js/script.js
@@ -6,3 +6,17 @@ window.addEventListener("DOMContentLoaded", function(){
         mobileMenu.classList.toggle("active");
     });
 });
+
+window.addEventListener("scroll", function(){
+    const scrollY = window.scrollY; 
+    const navbar = this.document.querySelector(".navbar");
+    
+    if(scrollY > 0){
+        navbar.classList.add("navbar--scroll");
+    }
+    else{
+        navbar.classList.remove("navbar--scroll");
+    }
+
+    console.log(navbar);
+});
