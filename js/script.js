3d9cc2b56d0c931271e028ceb9a2a2fd0bd9811e
Tomicz Engineering LLC <tomiczdarko@gmail.com>
2024-09-10 20:34:37 +0200

    final commit

--- js/script.js
+++ js/script.js
@@ -1,12 +1,38 @@
 window.addEventListener("DOMContentLoaded", function(){
+    // Mobile menu
     const toggleButton = this.document.querySelector(".navbar__mobile-menu-toggle");
     const mobileMenu = this.document.querySelector(".navbar__mobile-menu-items");
 
     toggleButton.addEventListener("click", function(){
         mobileMenu.classList.toggle("active");
     });
+
+    // Video modal
+
+    const modal = this.document.getElementById("videoModal");
+    const videoButton = this.document.querySelector(".preview__video-button");
+    const closeButton = this.document.querySelector(".modal__close-button");
+    const videoPlayer = this.document.getElementById("videoPlayer")
+
+    // Open modal when clicked
+    videoButton.addEventListener("click", function(){
+        // Show modal
+        modal.style.display = block;
+
+        // Replace src attribute with video url
+        videoPlayer.src = "https://www.youtube.com/embed/8sXRyHI3bLw"
+
+        // Close modal
+        closeButton.addEventListener("click", function(){
+            modal.style.display = "none";
+            videoPlayer.src = "";
+        });
+    });
+
+
 });
 
+// Navigation background on scroll
 window.addEventListener("scroll", function(){
     const scrollY = window.scrollY; 
     const navbar = this.document.querySelector(".navbar");
